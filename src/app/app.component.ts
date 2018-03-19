e29c527319b05d8421a3be0855c767304c7671a8
Future Front-End Developer <grzegorz.stanczyk@o2.pl>
2018-03-19 19:46:15 +0100

    First sketch of drag and drop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+
+  drag(event, data) {
+    console.log('data: ', data);
+    console.log('event: ', event);
+    event.dataTransfer.setData('text', event.target.id);
+  }
+
+  onDrop(event) {
+    event.preventDefault();
+    const dataTransfer = event.dataTransfer.getData('text');
+    event.target.appendChild(document.getElementById(dataTransfer));
+  }
+
+  allowDrop(event) {
+    event.preventDefault();
+  }
 }
